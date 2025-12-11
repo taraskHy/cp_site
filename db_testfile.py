07c7b2d48ef00bc4a77822e3d8b9abec48030de7
taraskHy <tarask.hyatt.il@gmail.com>
2025-12-11 21:27:06 +0200

    Update db_testfile.py

--- db_testfile.py
+++ db_testfile.py
@@ -3,15 +3,15 @@ import pymysql
 timeout = 10
 connection = pymysql.connect(
     charset="utf8mb4",
-    connect_timeout=timeout,
-    cursorclass=pymysql.cursors.DictCursor,
-    db="defaultdb",
-    host="mysql-61299e7-tomerh1307-848d.b.aivencloud.com",
-    password="AVNS_IaSLgu0RmDRZK6CsuO4",
-    read_timeout=timeout,
-    port=28557,
-    user="avnadmin",
-    write_timeout=timeout,
+        connect_timeout=timeout,
+        cursorclass=pymysql.cursors.DictCursor,
+        db="defaultdb",
+        host="mysql-cpuoh2026-cpuoh2026.h.aivencloud.com",
+        password="AVNS_6_akBbjIQV3juRmmGY6",
+        read_timeout=timeout,
+        port=16235,
+        user="avnadmin",
+        write_timeout=timeout,
 )
 
 try:
