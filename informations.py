d64761b153b2e721b53529b92df2c4846cb96c44
taraskHy <tarask.hyatt.il@gmail.com>
2025-12-22 21:35:46 +0200

    Update informations.py

--- informations.py
+++ informations.py
@@ -1,9 +1,10 @@
 import json
 import db_handler
 
-with db_handler.load_db() as data:
-    i = data['usernames']
-    l = [[user.get(str(c), 2) for c in range(5)] + [struser, user['etgar']] for struser, user in i.items()]
+data = db_handler.load_db()
+if data:
+    i = data.get('usernames', {})
+    l = [[user.get(str(c), 2) for c in range(5)] + [struser, user.get('etgar')] for struser, user in i.items()]
     for user in l:
         c = user.count(0)
         if c > 0:
