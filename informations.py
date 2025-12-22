acb0d433d8ec03cd2ba37e876896487ac50ee67c
taraskHy <tarask.hyatt.il@gmail.com>
2025-12-22 21:59:55 +0200

    Update informations.py

--- informations.py
+++ informations.py
@@ -3,8 +3,8 @@ import db_handler
 
 data = db_handler.load_db()
 if data:
-    i = data.get('usernames', {})
-    l = [[user.get(str(c), 2) for c in range(5)] + [struser, user.get('etgar')] for struser, user in i.items()]
+    i = data['usernames']
+    l = [[user.get(str(c), 2) for c in range(5)] + [struser, user['etgar']] for struser, user in i.items()]
     for user in l:
         c = user.count(0)
         if c > 0:
