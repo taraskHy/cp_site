6ee3270f811300b9d6466e871a6b4b33a517982a
ethan <tarask.hyatt.il@gmail.com>
2026-03-10 16:23:12 +0200

    fix not register 2167Cf

--- data.py
+++ data.py
@@ -110,6 +110,6 @@ total_prob = week1u + week1l + week2u + week2l + week3u + week3l + week4u + week
 '''
 stars = week0s + week1s
 total_prob = week0u + week0l + week1u + week1l
-total_cf = ['4A','103081E','545C','1472C','2167Cf','2094D','698A','2167G'] #list of Codeforces problem IDs
+total_cf = ['4A','103081E','545C','1472C','2167C','2094D','698A','2167G'] #list of Codeforces problem IDs
 admins = ['tomer1307', 'yarinys', 'yarin', 'ethan', 'shalev']
 black = ['all', 'omer1308', 'yaser2408', 'ethanh', 'tomer']
