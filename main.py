703a98de0bbd46b46cffd1da3c8fc54b91430d33
ethan <tarask.hyatt.il@gmail.com>
2026-08-03 20:19:13 +0300

    week 2 solution

--- main.py
+++ main.py
@@ -214,6 +214,26 @@ if st.session_state.get('authentication_status') and st.session_state.get('reg')
             st.header("Week two - Graphs")
             new_off = week(week2u, week2l, week2s, tasks, new_off)
             st.write("---")
+            st.markdown(
+                """
+                <div style="
+                    color: red;
+                    font-size: 2rem;
+                    font-weight: 600;
+                    line-height: 1.2;
+                    margin: 0.25rem 0 1rem 0;
+                ">
+                    Week Two - Homework Solution:
+                </div>
+                """,
+                unsafe_allow_html=True
+            )
+            path = "S/Solutions/Week_2/main.cpp"
+            bo = BytesIO(open(path, 'rb').read())
+            st.download_button(label='week 2 - HW solution', data=bo.getvalue(), file_name='main.cpp',
+                               mime='text/x-c++src', key='cpw11825650')
+            st.write("---")
+
 
         with st.container():
             if not st.session_state.get('authentication_status'):
