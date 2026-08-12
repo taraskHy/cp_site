2142529094a4d1ec1f0cce184b0b886f7cbdae74
ethan <tarask.hyatt.il@gmail.com>
2026-08-12 20:08:45 +0300

    add week5 mo

--- data.py
+++ data.py
@@ -93,13 +93,16 @@ week4l = [
     ['2206s', 'https://cses.fi/problemset/task/2206', 's'],  # locked/bonus - 3 stars
 ]
 
-week5s = [0, 1, 0, 0, 1]
+week5s = [0, 1, 1, 0, 1, 0, 1, 1]
 week5u = [
     ['1138s', 'https://cses.fi/problemset/task/1138', 's'],
     ['1006Ef', 'https://codeforces.com/problemset/problem/1006/E', 'f'],
+    ['220Bf', 'https://codeforces.com/problemset/problem/220/B', 'f'],
     ['1135s', 'https://cses.fi/problemset/task/1135', 's'],
+    ['617Ef', 'https://codeforces.com/problemset/problem/617/E', 'f'],
     ['1137s', 'https://cses.fi/problemset/task/1137', 's'],
     ['1328Ef', 'https://codeforces.com/problemset/problem/1328/E', 'f'],
+    ['86Df', 'https://codeforces.com/problemset/problem/86/D', 'f'],
 ]
 week5l = [
 
@@ -213,6 +216,7 @@ total_prob = (week0u + week0l + week1u + week1l + week1hw + week1hwl + week2u + 
 total_cf = ['4A','1669F', '1374C', '2167B', '2167C', '2094D', '2167G',
             '443A', '996A', '160A', '34B', '1353B', '4C', '1263D',
             '20C', '475B', '277A', '510C', '520B', '1167C', '427C', '938D',
-            '653D', '339D', '52C', '380C', '1234D', '1006E', '1328E'] #list of Codeforces problem IDs
+            '653D', '339D', '52C', '380C', '1234D', '1006E', '1328E',
+            '220B', '617E', '86D'] #list of Codeforces problem IDs
 admins = ['tomer1307', 'yarinys', 'yarin', 'ethan', 'shalev']
 black = ['all', 'omer1308', 'yaser2408', 'ethanh', 'tomer', 'markin2000']
